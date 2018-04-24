805e956c21b0ed63d2cbd17a795a06564943b0b4
Ilya Zverev <zverik@textual.ru>
2018-04-25 00:25:31 +0300

    More work on data

--- level0/data.py
+++ level0/data.py
@@ -1,20 +1,62 @@
+try:
+    from lxml import etree
+except ImportError:
+    import xml.etree.ElementTree as etree
+
+
 class Entity(object):
-    def __init__(self, typ, oid, version=None, action=None):
-        self.typ = typ
-        self.oid = oid
-        self.version = version
-        self.action = action
+    def __init__(self, typ, oid):
+        self.type = typ
+        self.id = oid
+        self.version = None
+        self.action = None
         self.members = []
         self.lat = None
         self.lon = None
         self.tags = {}
-        if oid > 0:
-            self.base = self.copy()
-        else:
-            self.base = None
+        self.conflict = None
+
+    @staticmethod
+    def from_xml(el, action=None):
+        e = Entity(el.tag, int(el.get('id')))
+        v = el.get('version', None)
+        if v:
+            e.version = int(v)
+        e.action = action or el.get('action', None)
+        for tag in el.findall('tag'):
+            e.tags[tag.get('k')] = tag.get('v')
+        if e.type == 'node':
+            e.lat = float(el.get('lat'))
+            e.lon = float(el.get('lon'))
+        elif e.type == 'way':
+            for nd in el.findall('nd'):
+                e.members.append(int(nd.get('ref')))
+        elif e.type == 'relation':
+            for m in el.findall('member'):
+                e.members.append((m.get('type'), int(m.get('ref')), m.get('role', '')))
+        return e
+
+    def to_xml(self, osc=False):
+        el = etree.Element(self.type, id=str(self.id))
+        if self.version:
+            el.set('version', str(self.version))
+        if self.action and not osc:
+            el.set('action', self.action)
+        if self.type == 'node':
+            el.set('lat', str(self.lat))
+            el.set('lon', str(self.lon))
+        elif self.type == 'way':
+            for nd in self.members:
+                etree.SubElement(el, 'nd', ref=str(nd))
+        elif self.type == 'relation':
+            for m in self.members:
+                etree.SubElement(el, 'member', type=m[0], ref=str(m[1]), role=m[2] or '')
+        for k, v in self.tags.items():
+            etree.SubElement(el, 'tag', k=k, v=v)
+        return el
 
     def copy(self):
-        res = Entity(self.typ, self.oid, self.version, self.action)
+        res = Entity(self.type, self.id, self.version, self.action)
         res.members = []
         for m in self.members:
             res.members.append(list(m))
@@ -23,27 +65,27 @@ class Entity(object):
         res.tags = dict(self.tags)
         return res
 
-    def is_modified(self):
-        if self.action or self.oid <= 0:
+    def is_modified(self, base):
+        if self.action or self.id <= 0:
             return True
-        if not self.base:
+        if not base:
             return False
-        if self.version > self.base.version:
+        if self.version > base.version:
             return True
-        if self.tags != self.base.tags:
+        if self.tags != base.tags:
             return True
-        if self.members != self.base.members:
+        if self.members != base.members:
             return True
-        if self.lat != self.base.lat or self.lon != self.base.lon:
+        if self.lat != base.lat or self.lon != base.lon:
             return True
         return False
 
     def sort_key(self):
-        if self.typ == 'node':
+        if self.type == 'node':
             g = 0
-        elif self.typ == 'way':
+        elif self.type == 'way':
             g = 1
-        elif self.typ == 'relation':
+        elif self.type == 'relation':
             g = 2
         else:
             g = -1
@@ -51,4 +93,11 @@ class Entity(object):
             g = 22 - g
         elif self.action == 'modify':
             g = 10 + g
-        return (g, self.oid)
+        return (g, self.id)
+
+    def __lt__(self, other):
+        return self.sort_key() < other.sort_key()
+
+    def __eq__(self, other):
+        # TODO: multiple created objects???
+        return self.sort_key() == other.sort_key()
--- level0/level0l.py
+++ level0/level0l.py
@@ -1,9 +1,61 @@
-import json
+from data import Entity
 
 
-def to_l0l(data):
-    pass
+def userdata_cmp(e):
+    if e.type == 'node':
+        grade = 0 if e.tags else 4
+    elif e.type == 'way':
+        grade = 1
+    elif e.type == 'relation':
+        grade = 2
+    else:
+        grade = -1
+    return (grade, e.id)
 
 
-def from_l0l(data):
-    pass
+def to_l0l(data, fileobj):
+    def short_type(t):
+        if t[0] == 'n':
+            return 'nd'
+        elif t[0] == 'w':
+            return 'wy'
+        return 'rel'
+
+    need_nl = False
+    tmp_id = 0
+    for e in sorted(data, key=userdata_cmp):
+        tmp_id += 1
+        if need_nl or e.type != 'node':
+            fileobj.write('\n')
+        need_nl = e.type != 'node' or e.tags or e.conflict
+
+        # TODO: conflict
+
+        if e.action == 'delete':
+            fileobj.write('-')
+        fileobj.write(e.type)
+        if e.id != 0:
+            fileobj.write(' {}'.format(e.id))
+        if e.type == 'node' and e.lat and e.lon:
+            fileobj.write(': {}, {}'.format(e.lat, e.lon))
+        fileobj.write('\n')
+        for k, v in e.tags.items():
+            fileobj.write('  {} = {}\n'.format(k.replace('=', '\\='), v))
+        if e.type == 'way':
+            for nd in e.members:
+                fileobj.write('  nd {}\n'.format(nd))
+        elif e.type == 'relation':
+            for m in e.members:
+                fileobj.write('  {} {}{}\n',
+                              short_type(m[0]),
+                              m[1],
+                              '' if not m[2] else ' ' + m[2])
+
+
+def from_l0l(fileobj, base):
+    data = []
+    line_no = 0
+    for line in fileobj:
+        line_no += 1
+        # TODO
+    return sorted(data)
